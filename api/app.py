c84e78f8e458ee9fd055fc9850f714218726fe9c
Jonathan Zhang <jonathan.z99@gmail.com>
2020-07-05 21:33:57 -0700

    added handlers

--- api/app.py
+++ api/app.py
@@ -6,6 +6,7 @@ from api.blueprints.review import review_blueprint
 
 from api.config import configs
 from api.data import db
+from api.utils import register_auth_error_handlers
 
 
 def create_app(config=None):
@@ -21,5 +22,6 @@ def create_app(config=None):
     app.register_blueprint(review_blueprint, url_prefix='/api/review')
 
     db.init_app(app)
+    register_auth_error_handlers(app)
 
     return app
