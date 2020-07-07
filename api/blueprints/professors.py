0575925839f36e387e88d6897a59637a9ee185ef
Jonathan Zhang <jonathan.z99@gmail.com>
2020-07-06 22:57:00 -0700

    changed example professors pathway (#14)

--- api/blueprints/professors.py
+++ api/blueprints/professors.py
@@ -1,13 +1,13 @@
 import flask
 
-import api.data.professors_loader as professors_loader
+from api.data.professors_loader import get_all_professors
 
 professors_blueprint = flask.Blueprint('professors_blueprint', __name__)
 
 
 @professors_blueprint.route('/all', methods=['POST'])
 def all_professors():
-    professors = professors_loader.get_all_professors()
+    professors = get_all_professors()
     professors_json = [{
         'firstName': professor['first_name'],
         'lastName': professor['last_name']
