dba4bb718ae14d368ca5fa47e1727bcda4f09028
Jin Woo Won <jw3580@columbia.edu>
2020-07-08 01:24:07 -0700

    First version adapted from sidebar example

---
+++ src/components/Announcements.jsx
@@ -0,0 +1,40 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+import ErrorComponent from "components/common/ErrorComponent";
+import LoadingComponent from "components/common/LoadingComponent";
+import useDataFetch from "components/common/useDataFetch";
+import Sidebar from "components/common/Sidebar";
+
+const propTypes = {
+  messages: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export function AnnouncementsSection(props) {
+  // TODO: Implement Announcements
+  const { messages } = props;
+  return <div>{messages}</div>;
+}
+
+AnnouncementsSection.propTypes = propTypes;
+
+export default function Announcements() {
+  const {
+    data: { messages },
+    isLoading,
+    isError,
+  } = useDataFetch("/api/announcements/all", {
+    messages: [],
+  });
+
+  if (isLoading || isError) {
+    return isLoading ? <LoadingComponent /> : <ErrorComponent />;
+  }
+
+  return (
+      <div>
+        <Sidebar />
+        <AnnouncementsSection messages={messages} />)
+      </div>
+  );
+}
