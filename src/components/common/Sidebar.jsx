df5c2f23d0396fd26d62937637553184ddaeb432
Jin Woo Won <jw3580@columbia.edu>
2020-07-08 22:00:07 -0700

    Implement rudimentary sidebar as ul

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,45 +1,22 @@
-/* Adapted from https://react.semantic-ui.com/modules/sidebar/#states-dimmed */
-// import { useBooleanKnob } from '@stardust-ui/docs-components'    // issue with prettier version (babel/parser issue)
-import React from 'react'
-import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
-const DimmingSidebar = () => {
-//   const [visible, setVisible] = useBooleanKnob({ name: 'visible' })
+export default class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: false};
+  }
 
-  return (
-    <Sidebar.Pushable as={Segment}>
-      <Sidebar
-        as={Menu}
-        animation='overlay'
-        icon='labeled'
-        inverted
-        // onHide={() => setVisible(false)}
-        vertical
-        // visible={visible}
-        width='thin'
-      >
-        <Menu.Item as='a'>
-          <Icon name='home' />
-          Home
-        </Menu.Item>
-        <Menu.Item as='a'>
-          <Icon name='gamepad' />
-          Games
-        </Menu.Item>
-        <Menu.Item as='a'>
-          <Icon name='camera' />
-          Channels
-        </Menu.Item>
-      </Sidebar>
-
-      <Sidebar.Pusher /*dimmed={visible}*/ >
-        <Segment basic>
-          <Header as='h3'>Application Content</Header>
-          <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-        </Segment>
-      </Sidebar.Pusher>
-    </Sidebar.Pushable>
-  )
+  render() {
+    return (
+      <div>
+        <p>Sidebar</p>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/professors">Professors</Link></li>
+          <li><Link to="/review">Reviews</Link></li>
+        </ul>
+      </div>
+    );
+  }
 }
-
-export default DimmingSidebar
