54a71bec860a2394e2e199a449d9d8df7efe1982
Jin Woo Won <jw3580@columbia.edu>
2020-07-08 22:01:45 -0700

    Fix import and comment out isToggleOn

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 export default class Sidebar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isToggleOn: false};
+    // this.state = {isToggleOn: false};
   }
 
   render() {
