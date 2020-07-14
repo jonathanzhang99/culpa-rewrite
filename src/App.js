71d8aee94246fd6de89ebd61516772198bab4165
Jonathan Zhang <jonathan.z99@gmail.com>
2020-07-14 15:02:03 -0700

    [Frontend Authentication PART 2] Installed Semantic UI (#18)
    * installed semantic ui
    
    * [Frontend Authentication PART 3] Removed Announcements since not in Design (#19)
    
    * removed old examples
    
    * removed announcements from app.js
    
    * removed announcements path
    
    * [Frontend Authentication PART 4 - IMPORTANT] Added Authentication Context (#20)
    
    * added authentication context
    
    * [Frontend Authentication PART 5] Updated Data Fetch to include Credentials (#21)
    
    * useDataFetch updated with authentication headers
    
    * [Frontend Authentication PART 6 - IMPORTANT] Synced Inputs and Forms with Semantic UI (#22)
    
    * synced forms with semantic ui
    
    * [Frontend Authentication PART 7] FINALLY created Login Page (#23)
    
    * included login routes
    
    * added protected route

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
-import Announcements from "components/Announcements";
+import { AuthProvider, ProtectedRoute } from "components/common/Authentication";
+import Login from "components/Login";
 import Professors from "components/Professors";
 import ReviewForm from "components/Review";
 
 function App() {
   return (
-    <div>
-      {/* <NavigationBar /> */}
-      <Router>
-        <Switch>
-          <Route path="/review">
-            <ReviewForm />
-          </Route>
-          <Route path="/professors">
-            <Professors />
-          </Route>
-          <Route path="/">
-            <Announcements />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Container>
+      <AuthProvider>
+        {/* <NavigationBar /> */}
+        <Router>
+          <Switch>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <ProtectedRoute exact path="/admin">
+              <div>
+                <h1>Admin only page!!</h1>
+              </div>
+            </ProtectedRoute>
+            <Route path="/review">
+              <ReviewForm />
+            </Route>
+            <Route path="/professors">
+              <Professors />
+            </Route>
+            <Route path="/">
+              <div>
+                <h1>Welcome to CULPA: Temporary header</h1>
+              </div>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
+    </Container>
   );
 }
 
