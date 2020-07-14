6287fbf6eae057e09f4fffe813ccda7c38dda8c9
Jonathan Zhang <jonathan.z99@gmail.com>
2020-07-14 14:53:34 -0700

    [Frontend Authentication PART 6 - IMPORTANT] Synced Inputs and Forms with Semantic UI (#22)
    * synced forms with semantic ui
    
    * [Frontend Authentication PART 7] FINALLY created Login Page (#23)
    
    * included login routes
    
    * added protected route

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -1,6 +1,9 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { Form as SemanticForm, Message } from "semantic-ui-react";
+
+const FORM_ERRORS = "form";
 
 const propTypes = {
   defaultValues: PropTypes.objectOf(PropTypes.any),
@@ -9,6 +12,7 @@ const propTypes = {
     PropTypes.element,
   ]),
   onSubmit: PropTypes.func.isRequired,
+  onSuccess: PropTypes.func.isRequired,
 };
 
 const defaultProps = {
@@ -16,18 +20,48 @@ const defaultProps = {
   children: [],
 };
 
-export default function Form({ defaultValues, children, onSubmit }) {
-  const { handleSubmit, register } = useForm({ defaultValues });
+export default function Form({ defaultValues, children, onSubmit, onSuccess }) {
+  const { handleSubmit, register, errors, setError, clearErrors } = useForm({
+    defaultValues,
+  });
+
+  const onSubmitWithHandlers = async (data) => {
+    const submissionError = await onSubmit(data);
+    if (submissionError) {
+      setError(FORM_ERRORS, { type: "server", message: submissionError });
+    } else {
+      onSuccess(data);
+    }
+  };
+
+  const handleSubmitWithErrors = async () => {
+    clearErrors(FORM_ERRORS);
+    await handleSubmit(onSubmitWithHandlers)();
+  };
+
+  const registeredChildren = React.Children.map(children, (child) => {
+    const childError = errors[child.props.name];
+
+    let childErrorContent;
+    if (childError) {
+      childErrorContent = { content: childError.message };
+    }
+
+    if (React.isValidElement(child)) {
+      return React.cloneElement(child, {
+        register,
+        key: child.props.id,
+        error: childErrorContent,
+      });
+    }
+    return child;
+  });
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {React.Children.map(children, (child) => {
-        return React.cloneElement(child, {
-          register,
-          key: child.props.name,
-          ...child.props,
-        });
-      })}
-    </form>
+    <SemanticForm error={!!errors.form} onSubmit={handleSubmitWithErrors}>
+      <Message error header="Login Error" content={errors?.form?.message} />
+      {registeredChildren}
+    </SemanticForm>
   );
 }
 
