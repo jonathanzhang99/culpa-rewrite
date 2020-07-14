0337f42ca09ea9d14510fda21edd2694eb679e1c
Jonathan Zhang <jonathan.z99@gmail.com>
2020-07-14 00:03:49 -0700

    added authentication context

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
+import { AuthProvider } from "components/common/Authentication";
 import Professors from "components/Professors";
 import ReviewForm from "components/Review";
 
 function App() {
   return (
-    <div>
-      {/* <NavigationBar /> */}
-      <Router>
-        <Switch>
-          <Route path="/review">
-            <ReviewForm />
-          </Route>
-          <Route path="/professors">
-            <Professors />
-          </Route>
-          <Route path="/">
-            <div>
-              <h1>Welcome to CULPA: Temporary header</h1>
-            </div>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Container>
+      <AuthProvider>
+        {/* <NavigationBar /> */}
+        <Router>
+          <Switch>
+            <Route path="/review">
+              <ReviewForm />
+            </Route>
+            <Route path="/professors">
+              <Professors />
+            </Route>
+            <Route path="/">
+              <div>
+                <h1>Welcome to CULPA: Temporary header</h1>
+              </div>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
+    </Container>
   );
 }
 
