d2ce617148c1a02e2e9d323fb8c1b0c801a411c5
Jonathan Zhang <jonathan.z99@gmail.com>
2020-07-14 00:08:34 -0700

    added protected route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 
-import { AuthProvider } from "components/common/Authentication";
+import { AuthProvider, ProtectedRoute } from "components/common/Authentication";
 import Login from "components/Login";
 import Professors from "components/Professors";
 import ReviewForm from "components/Review";
@@ -17,6 +17,11 @@ function App() {
             <Route exact path="/login">
               <Login />
             </Route>
+            <ProtectedRoute exact path="/admin">
+              <div>
+                <h1>Admin only page!!</h1>
+              </div>
+            </ProtectedRoute>
             <Route path="/review">
               <ReviewForm />
             </Route>
