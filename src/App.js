73218e0cb2d28cc1e63bfb112fe7b52141f5057a
Sungbin Kim <sungbinkim98@gmail.com>
2020-07-14 19:59:04 -0400

    Add unstyled navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-import Announcements from "components/Announcements";
-import Professors from "components/Professors";
-import ReviewForm from "components/Review";
 import NavigationBar from "components/NavigationBar";
+import ReviewForm from "components/Review";
+import Professors from "components/Professors";
+import Announcements from "components/Announcements";
 
 function App() {
   return (
