ef2a33151c619317ca611785204b4329b5d6850d
Jin Woo Won <jw3580@columbia.edu>
2020-07-14 18:23:30 -0700

    Revert to component function due to the commented-out version does not require states and eslint complains: component needs to be a pure function

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -2,52 +2,41 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {  Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
-class CulpaSidebar extends React.Component {
-  constructor(props) {
-    super(props);
-    // this.setVisible = this.setVisible.bind(this);
-  }
+const CulpaSidebar = () => {
+  return (
+    <Sidebar.Pushable as={Segment}>
+      <Sidebar
+        as={Menu}
+        animation='overlay'
+        icon='labeled'
+        inverted
+        // onHide={() => this.setVisible(false)}
+        vertical
+        visible='true'
+        width='thin'
+        >
+          <Menu.Item as={ Link } name='home' to='/'>
+            <Icon name='home' />
+            Home
+          </Menu.Item>
+          <Menu.Item as={ Link } name='professors' to='/professors'>
+            <Icon name='address card' />
+            Professors
+          </Menu.Item>
+          <Menu.Item as={ Link } name='review' to='/review'>
+            <Icon name='edit outline' />
+            Reviews
+          </Menu.Item>
+        </Sidebar>
 
-  // setVisible(isVisible) {
-  //   this.setState({isVisible});
-  // }
-
-  render() {
-    return (
-      <Sidebar.Pushable as={Segment}>
-        <Sidebar
-          as={Menu}
-          animation='overlay'
-          icon='labeled'
-          inverted
-          // onHide={() => this.setVisible(false)}
-          vertical
-          visible='true'
-          width='thin'
-          >
-            <Menu.Item as={ Link } name='home' to='/'>
-              <Icon name='home' />
-              Home
-            </Menu.Item>
-            <Menu.Item as={ Link } name='professors' to='/professors'>
-              <Icon name='address card' />
-              Professors
-            </Menu.Item>
-            <Menu.Item as={ Link } name='review' to='/review'>
-              <Icon name='edit outline' />
-              Reviews
-            </Menu.Item>
-          </Sidebar>
-  
-          <Sidebar.Pusher>  {/* need a way to include actual content here */}
-            <Segment basic>
-              <Header as='h1'>Application Content</Header>
-              <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-            </Segment>
-          </Sidebar.Pusher>
-      </Sidebar.Pushable>
-    );
-  }
+        <Sidebar.Pusher>  {/* need a way to include actual content here */}
+          <Segment basic>
+            <Header as='h1'>Application Content</Header>
+            <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+          </Segment>
+        </Sidebar.Pusher>
+    </Sidebar.Pushable>
+  );
 
 }
 
