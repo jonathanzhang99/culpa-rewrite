56aadb1734a7483efefc5829b7b9c84014975f18
Sungbin Kim <sungbinkim98@gmail.com>
2020-07-15 00:01:08 -0400

    Add nonfunctional navbar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Menu, Search } from 'semantic-ui-react'
 
+import SearchBar from "components/SearchBar"
+
 export default function NavigationBar() {
     return (
         <Menu fixed="top">
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 // import { useForm } from "react-hook-form"
 
 import Form from "components/common/Form"
-import { Submit, Input } from "components/common/Inputs"
+import { Submit, TextInput } from "components/common/Inputs"
 
 function onSubmitForm() {
     // To be implemented
@@ -11,7 +11,7 @@ function onSubmitForm() {
 export default function SearchBar() {
     return (
         <Form onSubmit={onSubmitForm()}>
-            <Input name="search-bar" />
+            <TextInput name="search-bar" />
             <Submit value="Search" />
         </Form>  
     );
