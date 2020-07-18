05467feba8ecc359703d4c4f0a0e47f9bd645067
Sungbin Kim <sungbinkim98@gmail.com>
2020-07-18 01:31:15 -0400

    Integrate navbar and sidebar
    Navbar needs to wrap sidebar, and sidebar needs to wrap the site's contents. Therefore, moved sidebar from App.js into NavigationBar.jsx and passed everything to it as children. Because the site's contents are actually rendered in Sidebar.jsx, moved Container component into NavigationBar.jsx to separate its style from navbar's.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import { Container } from "semantic-ui-react";
 
 import { AuthProvider, ProtectedRoute } from "components/common/Authentication";
-import Sidebar from "components/common/Sidebar";
 import ReviewForm from "components/CreateReviewPage";
 import Login from "components/LoginPage";
 import NavigationBar from "components/NavigationBar";
@@ -12,36 +10,32 @@ import Professors from "components/ProfessorsPage";
 function App() {
   return (
     <AuthProvider>
-      <Container>
-        <Router>
+      <Router>
+        <NavigationBar>
           <Switch>
-              <Sidebar>
-                <NavigationBar>
-                  <Route exact path="/login">
-                    <Login />
-                  </Route>
-                  <ProtectedRoute exact path="/admin">
-                    <div>
-                      <h1>Admin only page!!</h1>
-                    </div>
-                  </ProtectedRoute>
-                  <Route path="/review">
-                    <ReviewForm />
-                  </Route>
-                  <Route path="/professors">
-                    <Professors />
-                  </Route>
-                  <Route path="/">
-                    <div>
-                      <h1>Welcome to CULPA: Temporary header</h1>
-                      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-                    </div>
-                  </Route>
-                </NavigationBar>
-              </Sidebar>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <ProtectedRoute exact path="/admin">
+              <div>
+                <h1>Admin only page!!</h1>
+              </div>
+            </ProtectedRoute>
+            <Route path="/review">
+              <ReviewForm />
+            </Route>
+            <Route path="/professors">
+              <Professors />
+            </Route>
+            <Route path="/">
+              <div>
+                <h1>Welcome to CULPA: Temporary header</h1>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+              </div>
+            </Route>
           </Switch>
-        </Router>
-      </Container>
+        </NavigationBar>
+      </Router>
     </AuthProvider>
   );
 }
--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,7 +1,7 @@
 import PropTypes, { oneOfType } from 'prop-types';
-import React, {useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import {  Button, Icon, Menu, Segment, Sidebar as SemanticSidebar } from 'semantic-ui-react'
+import { Menu, Segment, Sidebar as SemanticSidebar } from 'semantic-ui-react'
 
 const propTypes = {
   children: oneOfType([
@@ -14,58 +14,41 @@ const defaultProps = {
   children: [],
 };
 
-export default function Sidebar({ children }) {
-  const [isVisible, setVisible] = useState(false);
-
-  const handleClick = () => {
-    setVisible(!isVisible);
-  }
-
-  const handleSidebarHide = () => {
-    setVisible(false);
-  }
-
+export default function Sidebar({ children, handleSidebarHide, isVisible }) {
   return (
-    <div>
-      <Button onClick={handleClick}>
-        <Icon name='bars' />
-      </Button>
+    <div className="sidebar-container">
+      <SemanticSidebar
+        as={Menu}
+        animation='overlay'
+        compact
+        onHide={handleSidebarHide}
+        vertical
+        visible={isVisible}
+        width='thin'
+        >
+        <Menu.Item header as={ Link } name='welcome to culpa' to='/'>
+          WELCOME TO CULPA!
+        </Menu.Item>
+        <Menu.Item as={ Link } name='home' to='/'>
+          Home
+        </Menu.Item>
+        <Menu.Item as={ Link } name='about' to='/about'>
+          About
+        </Menu.Item>
+        <Menu.Item as={ Link } name='departments' to='/departments'>
+          Departments
+        </Menu.Item>
+        <Menu.Item as={ Link } name='join the team' to='/join-the-team'>
+          Join the Team
+        </Menu.Item>
+        <Menu.Item as={ Link } name='log in' to='/login'>
+          Login
+        </Menu.Item>
+      </SemanticSidebar>
       <SemanticSidebar.Pushable as={Segment}>
-        <SemanticSidebar
-          as={Menu}
-          animation='overlay'
-          icon='labeled'
-          compact
-          onHide={handleSidebarHide}
-          vertical
-          visible={isVisible}
-          width='thin'
-          >
-            <Menu.Item header as={ Link } name='welcome to culpa' to='/'>
-              WELCOME TO CULPA!
-              </Menu.Item>
-            <Menu.Item as={ Link } name='home' to='/'>
-              Home
-            </Menu.Item>
-            <Menu.Item as={ Link } name='about' to='/about'>
-              About
-            </Menu.Item>
-            <Menu.Item as={ Link } name='departments' to='/departments'>
-              Departments
-            </Menu.Item>
-            <Menu.Item as={ Link } name='join the team' to='/join-the-team'>
-              Join the Team
-            </Menu.Item>
-            <Menu.Item as={ Link } name='log in' to='/login'>
-              Login
-            </Menu.Item>
-          </SemanticSidebar>
-
-          <SemanticSidebar.Pusher dimmed={isVisible}>
-            <Segment basic>
-              {children}
-            </Segment>
-          </SemanticSidebar.Pusher>
+        <SemanticSidebar.Pusher>
+          {children}
+        </SemanticSidebar.Pusher>
       </SemanticSidebar.Pushable>
     </div>
   );
