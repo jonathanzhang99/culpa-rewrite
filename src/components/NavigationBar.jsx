166f2e38058d6deb34181be57f7a94df5ecba8bd
Sungbin Kim <sungbinkim98@gmail.com>
2020-07-18 01:31:15 -0400

    Integrate navbar and sidebar
    Navbar needs to wrap sidebar, and sidebar needs to wrap the site's contents. Therefore, moved sidebar from App.js into NavigationBar.jsx and passed everything to it as children. Because the site's contents are actually rendered in Sidebar.jsx, moved Container component into NavigationBar.jsx to separate its style from navbar's.

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,34 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Icon, Menu, Search } from 'semantic-ui-react'
+import { Button, Container, Icon, Menu, Search, Visibility } from 'semantic-ui-react'
+import PropTypes, { oneOfType } from 'prop-types';
 
 import "../styles/navigationBar.css";
 
-export default function NavigationBar() {
-    return (
-        <Menu fixed="top">
-            <Menu.Item
-                className="sidebar-tab-container"
-            > 
-                <Icon name="bars" fitted size="big" color="blue"/>
-            </Menu.Item>
-            <Menu.Item
-                as={Link} to="/"
-                className="culpa-logo-container"
-            >
-                <h1> CULPA </h1>
-            </Menu.Item>
-            <Menu.Item
-                className="searchbar-container"
-            >
-                <Search className="searchbar" fluid placeholder="Search for professors or courses." />
-            </Menu.Item>
-            <Menu.Item
-                as={Link} to="/review"
-                className="review-button-container"
-            >
-                <Button className="review-button" color="yellow"> WRITE A REVIEW </Button>
-            </Menu.Item>
+import Sidebar from "components/common/Sidebar";
+
+const propTypes = {
+  children: oneOfType([
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.element,
+  ])
+};
+
+const defaultProps = {
+  children: [],
+};
+
+export default function NavigationBar({ children }) {
+
+  const [isVisible, setVisible] = useState(false);
+  const [isNavbarFixed, setNavbarFixed] = useState(false);
+
+  const handleClick = () => {
+    setVisible(!isVisible);
+  }
+
+  const handleSidebarHide = () => {
+    setVisible(false);
+  }
+
+  const fixNavbar = () => {
+    setNavbarFixed(true);
+  }
+
+  const unfixNavbar = () => {
+    setNavbarFixed(false);
+  }
+
+  return (
+    <div className="navbar-container">
+      <Visibility
+        onTopPassed={fixNavbar}
+        onTopVisible={unfixNavbar}
+        once={false}
+      >
+        <Menu
+          fixed={isNavbarFixed ? "top" : undefined}
+        >
+          <Menu.Item
+            className="sidebar-tab-container"
+          > 
+            <Button onClick={handleClick} basic>
+              <Icon name='bars' fitted size="big" color="blue" />
+            </Button>
+          </Menu.Item>
+          <Menu.Item
+            as={Link} to="/"
+            className="culpa-logo-container"
+          >
+            <h1> CULPA </h1>
+          </Menu.Item>
+          <Menu.Item
+            className="searchbar-container"
+          >
+            <Search className="searchbar" fluid placeholder="Search for professors or courses." />
+          </Menu.Item>
+          <Menu.Item
+            as={Link} to="/review"
+            className="review-button-container"
+          >
+            <Button className="review-button" color="yellow"> WRITE A REVIEW </Button>
+          </Menu.Item>
         </Menu>
-    );
+      </Visibility>
+      <Container>
+        <Sidebar
+          handleSidebarHide={handleSidebarHide}
+          isVisible={isVisible}
+        >
+          {children}
+        </Sidebar>
+      </Container>
+    </div>
+  );
 }
+
+NavigationBar.propTypes = propTypes;
+NavigationBar.defaultProps = defaultProps;
+>>>>>>> Integrate navbar and sidebar
