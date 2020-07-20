f7fde9a2a9145bccb61b3be63e2a03c933254aa4
Jonathan Zhang <jonathan.z99@gmail.com>
2020-07-20 00:16:12 -0700

    removed announcements from backend (#26)

--- api/tests/blueprint_tests/test_announcements.py
+++
@@ -1,8 +0,0 @@
-from api.tests import BaseTest
-
-
-class AnnouncementsTest(BaseTest):
-    def test_retrieve_all_announcements(self):
-        expected_res = {'messages': ['message from server']}
-        res = self.app.post('/api/announcements/all')
-        self.assertEqual(expected_res, res.json)
