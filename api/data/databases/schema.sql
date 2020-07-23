4d3b53127f2c174afe9c85dc5792cbeb54f1b262
Jin Woo Won <jw3580@columbia.edu>
2020-07-23 15:14:44 -0700

    Change all primary keys to 'id'

--- api/data/databases/schema.sql
+++ api/data/databases/schema.sql
@@ -1,81 +1,81 @@
 CREATE TABLE `department` (
-  `department_id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`department_id`)
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `announcement` (
-  `announcement_id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `content` text CHARACTER SET UTF8MB4,
-  PRIMARY KEY (`announcement_id`)
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `course` (
-  `course_id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `department_id` INT NOT NULL,
-  PRIMARY KEY (`course_id`),
-  FOREIGN KEY (`department_id`) REFERENCES `department`(`department_id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`department_id`) REFERENCES `department`(`id`)
 );
 
 CREATE TABLE `course_instance` (
-  `instance_id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `year` YEAR NULL,
   `semester` INT NULL,
   `course_id` INT NULL,
-  PRIMARY KEY (`instance_id`),
-  FOREIGN KEY(`course_id`) REFERENCES `course` (`course_id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`course_id`) REFERENCES `course` (`id`)
 );
 
 CREATE TABLE `professor` (
-  `professor_id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NULL,
   `last_name` VARCHAR(45) NULL,
-  PRIMARY KEY (`professor_id`)
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `professor_course` (
   `professor_id` INT NOT NULL,
   `instance_id` INT NOT NULL,
-FOREIGN KEY (`instance_id`) REFERENCES `course_instance` (`instance_id`),
-FOREIGN KEY (`professor_id`) REFERENCES `professor` (`professor_id`)
+FOREIGN KEY (`instance_id`) REFERENCES `course_instance` (`id`),
+FOREIGN KEY (`professor_id`) REFERENCES `professor` (`id`)
 );
 
 
 CREATE TABLE `department_professor`(
   `professor_id` INT NOT NULL,
   `department_id` INT NOT NULL,
-FOREIGN KEY (`professor_id`) REFERENCES `professor` (`professor_id`),
-FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`)
+FOREIGN KEY (`professor_id`) REFERENCES `professor` (`id`),
+FOREIGN KEY (`department_id`) REFERENCES `department` (`id`)
 );
 
 
 CREATE TABLE `review` (
-  `review_id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `instance_id` INT NOT NULL,
   `content` TEXT NULL,
   `rating` INT NULL,
   `submission_date` DATETIME NOT NULL,
   `professor_id` INT NOT NULL,
-  PRIMARY KEY (`review_id`),
-FOREIGN KEY (`instance_id`) REFERENCES `course_instance` (`instance_id`),
-FOREIGN KEY (`professor_id`) REFERENCES `professor` (`professor_id`)
+  PRIMARY KEY (`id`),
+FOREIGN KEY (`instance_id`) REFERENCES `course_instance` (`id`),
+FOREIGN KEY (`professor_id`) REFERENCES `professor` (`id`)
 );
 
 
 CREATE TABLE `vote` (
   `review_id` INT NOT NULL,
   `type` VARCHAR(255) NOT NULL,
-FOREIGN KEY (`review_id`) REFERENCES `review`(`review_id`)
+FOREIGN KEY (`review_id`) REFERENCES `review`(`id`)
 );
 
 CREATE TABLE `user` (
-  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL,
   `username` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   `privileges` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`user_id`)
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `flag` (
@@ -84,6 +84,6 @@ CREATE TABLE `flag` (
   `flag_type` VARCHAR(45) NULL,
   `created_at` DATETIME NOT NULL,
   `modified_at` DATETIME NOT NULL,
-FOREIGN KEY (`review_id`) REFERENCES `review` (`review_id`),
-FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+FOREIGN KEY (`review_id`) REFERENCES `review` (`id`),
+FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 );
