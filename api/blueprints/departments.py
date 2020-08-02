b1b7a05dfa71558b7d5f60f95e802984de4feab4
Yuanyuting Wang <yw3241@columbia.edu>
2020-08-02 00:45:24 -0400

    py linter

--- api/blueprints/departments.py
+++ api/blueprints/departments.py
@@ -4,6 +4,7 @@ from api.data.dataloaders.departments_loader import get_all_departments
 
 departments_blueprint = flask.Blueprint('departments_blueprint', __name__)
 
+
 @departments_blueprint.route('/all', methods=['GET'])
 def all_departments():
     departments = get_all_departments()
--- api/blueprints/votes.py
+++ api/blueprints/votes.py
@@ -4,20 +4,23 @@ from api.data.datasetters.votes_setter import add_vote, revoke_vote
 
 votes_blueprint = flask.Blueprint('votes_blueprint', __name__)
 
+
 @votes_blueprint.route('/change', methods=['POST'])
 def change_vote():
     try:
         params = flask.request.json
-        
+
         action = params.get('action')
         voteType = params.get('voteType')
         reviewId = params.get('reviewId')
         ip = flask.request.remote_addr
 
         if action == "add":
-            add_vote(int(reviewId), voteType == 'upvote', voteType == 'downvote', voteType == 'funny', ip)
+            add_vote(int(reviewId), voteType == 'upvote',
+                     voteType == 'downvote', voteType == 'funny', ip)
         elif action == "revoke":
-            revoke_vote(int(reviewId), voteType == 'upvote', voteType == 'downvote', voteType == 'funny', ip)
+            revoke_vote(int(reviewId), voteType == 'upvote',
+                        voteType == 'downvote', voteType == 'funny', ip)
 
         return {"status": "success", "failure_msg": None}
 
