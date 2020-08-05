6dc9f189cdf342167b4bcb5620bdc9512770fb80
Pelin Cetin <35610616+pelincetin@users.noreply.github.com>
2020-08-06 01:58:40 +0300

    Alter the schema file (#40)
    * newest database schema file
    
    * alter the review table
    
    * change the character limit of callnumber and uni
    
    * new line
    
    * deleted professor id, added ip to review, changed to null
    
    * delete year, semester
    
    * renaming (#41)
    
    * used enums
    
    Co-authored-by: pelincetin <pc2807@barnard.edu>
    Co-authored-by: Jonathan Zhang <jonathan.z99@gmail.com>

--- api/data/common.py
+++ api/data/common.py
@@ -4,7 +4,7 @@ from pypika import Table
 department = Table('department')
 announcement = Table('announcement')
 course = Table('course')
-course_instance = Table('course_instance')
+course_professor = Table('course_professor')
 professor = Table('professor')
 department_professor = Table('department_professor')
 review = Table('review')
