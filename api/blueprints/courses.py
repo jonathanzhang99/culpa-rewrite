4f0f0ddc21a88bee654c625090cf6f0d61272ff8
Jin Woo Won <jw3580@columbia.edu>
2020-08-10 05:26:53 -0700

    Combine the four queries into two

--- api/blueprints/courses.py
+++ api/blueprints/courses.py
@@ -1,68 +1,57 @@
 from collections import defaultdict
 import flask
 
-import api.data.dataloaders.courses_loader as loader
+from api.data.dataloaders.courses_loader import get_course, get_professor_departments
 
 courses_blueprint = flask.Blueprint('courses_blueprint', __name__)
 
 
-@courses_blueprint.route('/all', methods=['GET'])
-def all_courses():
-    courses = loader.get_all_courses()
-    courses_json = [{
-        'name': course['name'],
-        'department_id': course['department_id']
-    } for course in courses]
-
-    return {'courses': courses_json}
-
-
 @courses_blueprint.route('/<course_id>', methods=['GET'])
 def course_summary(course_id):
-    course = loader.get_course(course_id)[0]
-    department = loader.get_department(course['department_id'])[0]
-    associated_professors = loader.get_prof_by_course(course_id)
-    professor_ids = [professor['professor_id']
-                     for professor in associated_professors]
-    departments = loader.get_department_by_prof(professor_ids)
+    # Fetch course info and all related professors
+    course = get_course(course_id)[0]
+    professor_departments = get_professor_departments(course_id)
 
-    # Convert to form {1: [{2, 'Computer Scinece'}]}, where 1 is professor_id
-    # and 2 is department_id
-    department_dict = defaultdict(list)
-    for d in departments:
-        for key, value in d.items():
-            if key == 'professor_id':
-                department_dict[value].append(
-                    {'department_id': d['department_id'],
-                     'name': d['name']})
-
-    # Include departments in associated professors
-    for professor in associated_professors:
-        professor_id = professor['professor_id']
-        professor['departments'] = department_dict[professor_id]
+    associated_professors = {}
+    for pd in professor_departments:
+        professor_id = pd['professor_id']
+        if pd['professor_id'] not in associated_professors.keys():
+            associated_professors[professor_id] = {
+                'first_name': pd['first_name'],
+                'last_name': pd['last_name'],
+                'departments': [{
+                    'department_id': pd['department_id'],
+                    'name': pd['name'],
+                }]
+            }
+        else:
+            # the professor belongs to multiple departments
+            associated_professors[professor_id]['departments'].append({
+                'department_id': pd['department_id'],
+                'name': pd['name'],
+            })
 
     # Structure of associated_professors:
-    #   associated_professor = [{'first_name': 'Nakul',
+    #   associated_professor = {1: {'first_name': 'Nakul',
     #                             'last_name': 'Verma',
-    #                             'professor_id': 1,
     #                             'departments': [{'department_id': 2,
     #                                             'name': 'Computer Science'},
     #                                            ... ]},
-    #                          ... ]
+    #                          ... }
 
     course_summary_json = {
         'courseName': course['name'],
         'courseCallNumber': course['call_number'],
         'departmentId': course['department_id'],
-        'departmentName': department['name'],
+        'departmentName': course['department_name'],
         'associatedProfessors': [{
             'firstName': professor['first_name'],
             'lastName': professor['last_name'],
-            'professorId': professor['professor_id'],
+            'professorId': professor_id,
             'profDepartments': [{
                 'profDepartmentId': department['department_id'],
                 'profDepartmentName': department['name']
             } for department in professor['departments']],
-        } for professor in associated_professors],
+        } for professor_id, professor in associated_professors.items()],
     }
     return {'courseSummary': course_summary_json}
--- api/data/dataloaders/courses_loader.py
+++ api/data/dataloaders/courses_loader.py
@@ -5,19 +5,6 @@ from api.data.common import course, course_professor, professor, \
     department, department_professor
 
 
-def get_all_courses():
-    cur = db.get_cursor()
-    query = Query.from_(course) \
-        .select(
-            course.course_id,
-            course.name,
-            course.department_id,
-            course.call_number,
-    ).get_sql()
-    cur.execute(query)
-    return cur.fetchall()
-
-
 def get_course(course_id):
     '''
     Query name, department_id from course table
@@ -28,22 +15,12 @@ def get_course(course_id):
             course.course_id,
             course.name,
             course.department_id,
-            course.call_number
-    ).where(
-            course.course_id == course_id
-    ).get_sql()
-    cur.execute(query)
-    return cur.fetchall()
-
-
-def get_department(department_id):
-    cur = db.get_cursor()
-    query = Query.from_(department) \
-        .select(
-            department.name
-    ).where(
-            department.department_id == department_id
-    ).get_sql()
+            course.call_number,
+            department.name.as_('department_name'),
+    ) \
+        .inner_join(department) \
+        .on(course.department_id == department.department_id) \
+        .where(course.course_id == course_id).get_sql()
     cur.execute(query)
     return cur.fetchall()
 
@@ -92,5 +69,26 @@ def get_department_by_prof(professor_ids):
             professor.professor_id.isin(professor_ids)
     ) \
         .get_sql()
+    cur.execute(query)
+    return cur.fetchall()
+
+
+def get_professor_departments(course_id):
+    cur = db.get_cursor()
+    query = Query.from_(course_professor) \
+        .select(
+            professor.professor_id,
+            professor.first_name,
+            professor.last_name,
+            department.department_id,
+            department.name,
+    ) \
+        .inner_join(professor) \
+        .on(course_professor.professor_id == professor.professor_id) \
+        .inner_join(department_professor) \
+        .on(professor.professor_id == department_professor.professor_id) \
+        .inner_join(department) \
+        .on(department_professor.department_id == department.department_id) \
+        .where(course_professor.course_id == course_id).get_sql()
     cur.execute(query)
     return cur.fetchall()
