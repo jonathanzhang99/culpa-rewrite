2d674f76bb9d01942eaab326bae83b54b708e369
Jonathan Zhang <jonathan.z99@gmail.com>
2020-08-21 16:06:29 -0700

    [Schema] Flag Type Update (#66)
    * schema update
    
    * removed modified at
    
    * flag primary key

--- api/data/databases/schema.sql
+++ api/data/databases/schema.sql
@@ -76,11 +76,17 @@ CREATE TABLE `user` (
 );
 
 CREATE TABLE `flag` (
+  `flag_id` INT NOT NULL AUTO_INCREMENT,
   `review_id` INT NOT NULL,
   `user_id` INT NOT NULL,
-  `flag_type` VARCHAR(45) NULL,
+  `type` ENUM(
+    'approved',
+    'pending',
+    'libel',
+    'insufficient'
+  ) NULL,
   `created_at` DATETIME NOT NULL,
-  `modified_at` DATETIME NOT NULL,
+  PRIMARY KEY(`flag_id`),
   CONSTRAINT `fk_flag__review` FOREIGN KEY (`review_id`)
     REFERENCES `review` (`review_id`),
   CONSTRAINT `fk_flag__user` FOREIGN KEY (`user_id`)
