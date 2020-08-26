abf886aaf9c05d7b69b3fd3a70efc34b9ea68f6a
Jin Woo Won <jw3580@columbia.edu>
2020-08-25 17:02:08 -0700

    Add reviews_loader tests for course review summary

--- api/tests/data_tests/common.py
+++ api/tests/data_tests/common.py
@@ -102,12 +102,18 @@ def setup_reviews_and_flags(cur):
     setup_users(cur)
 
     reviews = [
-        (2, 'demo content 1', '123.0.0.1', 'demo workload 1', 3, '2020-02-10'),
-        (2, 'demo content 2', '123.0.0.1', 'demo workload 2', 3, '2017-02-10'),
-        (3, 'demo content 3', '123.0.0.1', 'demo workload 3', 3, '2015-02-10'),
-        (5, 'demo content 4', '123.0.0.1', 'demo workload 4', 3, '2019-10-13'),
-        (5, 'demo content 5', '123.0.0.1', 'demo workload 5', 3, '2018-09-01'),
-        (5, 'demo content 6', '123.0.0.1', 'demo workload 6', 3, '2016-05-20'),
+        (2, 'demo content 1', '123.0.0.1',
+         'demo workload 1', 3, '2020-02-10'),  # review_id: 1
+        (2, 'demo content 2', '123.0.0.1',
+         'demo workload 2', 3, '2017-02-10'),  # review_id: 2
+        (3, 'demo content 3', '123.0.0.1',
+         'demo workload 3', 3, '2015-02-10'),  # review_id: 3
+        (5, 'demo content 4', '123.0.0.1',
+         'demo workload 4', 3, '2019-10-13'),  # review_id: 4
+        (5, 'demo content 5', '123.0.0.1',
+         'demo workload 5', 3, '2018-09-01'),  # review_id: 5
+        (5, 'demo content 6', '123.0.0.1',
+         'demo workload 6', 3, '2016-05-20'),  # review_id: 6
     ]
 
     cur.executemany(
@@ -176,7 +182,7 @@ def setup_for_course_test(cur):
     This function sets up review and vote tables to test
     courses_loader (to prevent breaking other tests)
     '''
-    setup_reviews_and_flags(cur)
+    setup_votes(cur)
 
     # Insert reviews for courses_loader testing
     reviews = [
@@ -194,10 +200,10 @@ def setup_for_course_test(cur):
         reviews
     )
 
-    setup_votes(cur)
-
     # Insert votes for courses_loader testing
     votes = [
+        (2, "123.456.78.910", "2018-08-11", "agree"),
+        (2, "123.456.78.910", "2018-08-11", "agree"),
         (7, "123.456.78.914", "2019-08-17", "agree"),
         (7, "123.456.78.914", "2019-08-17", "agree"),
         (7, "123.456.78.914", "2019-08-17", "agree"),
@@ -213,4 +219,37 @@ def setup_for_course_test(cur):
         'INSERT INTO vote (review_id, ip, created_at, type)'
         'VALUES (%s, %s, %s, %s)',
         votes
+    )
+
+    # Insert course without any reviews
+    cur.execute(
+        'INSERT INTO course (name, department_id, call_number)'
+        'VALUES (%s, %s, %s)',   # course_id: 7
+        ('course_without_review', 1, 'CWOR')
+    )
+    cur.execute(
+        'INSERT INTO course_professor (professor_id, course_id)'
+        'VALUES (%s, %s)',  # course_professor_id: 8
+        (1, 7)
+    )
+
+    # Insert course with reviews without any votes
+    cur.execute(
+        'INSERT INTO course (name, department_id, call_number)'
+        # course_id: 8
+        'VALUES (%s, %s, %s)',
+        ('course_with_reviews_without_votes', 1, 'CWRWV')
+    )
+    cur.execute(
+        'INSERT INTO course_professor (professor_id, course_id)'
+        'VALUES (%s, %s)',  # course_professor_id: 9
+        (1, 8)
+    )
+    cur.execute(
+        'INSERT INTO review'
+        '(course_professor_id, content, ip, workload, rating, submission_date)'
+        'VALUES'
+        '(%s, %s, %s, %s, %s, %s)',
+        (9, 'review_without_votes', '123.456,78.914',
+            'workload', 3, '2019-08-17')
     )
