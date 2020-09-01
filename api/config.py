a50e4570ba30e1de50b25225cf630ef599eb0616
Yuanyuting Wang <yw3241@columbia.edu>
2020-09-01 16:25:31 -0400

    fixed config.py

--- api/config.py
+++ api/config.py
@@ -10,7 +10,7 @@ class BaseConfig():
     MYSQL_DATABASE_USER = 'root'
     MYSQL_DATABASE_PASSWORD = None
     MYSQL_DATABASE_DB = 'culpa'
-    MYSQL_DATABASE_SOCKET = '/tmp/mysql.sock'
+    MYSQL_DATABASE_SOCKET = None
     MYSQL_DATABASE_CHARSET = 'utf8'
     MYSQL_USE_UNICODE = True
     MYSQL_USE_DICTCURSOR = True
